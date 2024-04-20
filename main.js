ac64f780bf427db6cb6d257d43f692c6f7a04e63
Rishabh18Arvind <153378447+Rishabh18Arvind@users.noreply.github.com>
2024-04-20 19:37:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log(Console);
+Concat(varfirst_name,varlast_name)
+console.log(My_first_console_project);
+console.log(Concat);
+var first_name = "Rishabh";
+var last_name = "Arvind";
+var a = 10;
+var b = 5;
+var add = a+b ;
+console.log(add);
+console.log(5);
+console.log(50);
+console.log(2);
